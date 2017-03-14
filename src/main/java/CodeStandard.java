05084b9bdbd85f694376a125fa0e5ca0f8c93595
ccbulage <1993zd2014@gmail.com>
2017-03-14 15:11:56 +0800

    代码格式

--- src/main/java/CodeStandard.java
+++ src/main/java/CodeStandard.java
@@ -2,4 +2,22 @@
  * Created by zd on 2017/3/10.
  */
 public class CodeStandard {
+
+    public void codeStyle(String paramA, String paramB){
+
+        if (1 == 1) {
+
+        } else {
+
+        }
+    }
+
+    public static void main(String[] args) {
+        String A = "A";
+        String B = "B";
+
+        codeStyle(A, B);
+
+        
+    }
 }
